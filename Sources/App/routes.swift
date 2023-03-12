53f856015f0949f7c356c7525a8c0d0684a74445
rommex <roman@unstoppabledomains.com>
2023-03-12 18:58:48 +0200

    added /subscribe and /unsubscribe

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,89 +1,55 @@
 import Vapor
 
-var count = 1
-
-// Define a Book struct to hold book data
-struct Book: Content {
-    let id: Int
-    let title: String
-    let author: String
+struct TxsSubscriptions {
+    private var subs: [String: [String]] = [:]
+    
+    mutating func addNew(subscription: [String]) -> String {
+        let newToken = UUID().uuidString
+        subs[newToken] = subscription
+        return newToken
+    }
+    
+    mutating func remove(token: String) -> [String]? {
+        subs.removeValue(forKey: token)
+    }
 }
 
-// Define an Author struct to hold author data
-struct Author: Content {
-    let name: String
-    let bio: String
+struct WalletList: Content {
+    let wallets: [String]
 }
 
-// Create a list of example books
-var books = [
-    Book(id: 1, title: "To Kill a Mockingbird", author: "Harper Lee"),
-    Book(id: 2, title: "1984", author: "George Orwell"),
-    Book(id: 3, title: "The Catcher in the Rye", author: "J.D. Salinger")
-]
-
-// Create a list of example authors
-var authors = [
-    Author(name: "Harper Lee", bio: "Nelle Harper Lee was an American novelist best known for her 1960 novel To Kill a Mockingbird."),
-    Author(name: "George Orwell", bio: "Eric Arthur Blair, better known by his pen name George Orwell, was an English novelist and essayist."),
-    Author(name: "J.D. Salinger", bio: "Jerome David Salinger was an American writer best known for his 1951 novel The Catcher in the Rye.")
-]
+var subscriptions: TxsSubscriptions = TxsSubscriptions()
 
 func routes(_ app: Application) throws {
-    app.get("book", ":id") { req -> Book in
-        // Get the book ID from the request parameters
-        guard let idString = req.parameters.get("id"),
-              let id = Int(idString)
-        else {
+    
+    // API for UD:
+    // POST /ud/txs/subscribe { wallets: ["0x193497394"] } -> token
+    // GET GET ud/txs/unsubscribe/<:token>
+    // GET ud/txs/hash/<:token>
+    
+    app.get("ud", "txs", "subscribe") { req -> String in
+        guard let wallets = try? parseWalletList(req) else {
+            throw Abort(.badRequest)
+        }
+        let token = subscriptions.addNew(subscription: wallets)
+        
+        DispatchQueue.global().async {
+            refreshHash(for: token)
+        }
+        
+        return token
+    }
+        
+    app.get("ud", "txs", "unsubscribe", ":token") { req -> [String] in
+        guard let token = req.parameters.get("token") else {
             throw Abort(.badRequest)
         }
         
-        // Find the book with the matching ID
-        guard let book = books.first(where: { $0.id == id })
-        else {
-            throw Abort(.notFound)
-        }
-        
-        // Return the book
-        return book
+        let removed = subscriptions.remove(token: token)
+        return removed ?? []
     }
     
-    app.get("book", ":id", "author") { req -> Author in
-        // Get the book ID from the request parameters
-        guard let idString = req.parameters.get("id"),
-              let id = Int(idString)
-        else {
-            throw Abort(.badRequest)
-        }
-        
-        // Find the book with the matching ID
-        guard let book = books.first(where: { $0.id == id })
-        else {
-            throw Abort(.badRequest)
-        }
-        
-        guard let author = authors.first(where: { $0.name == book.author })
-        else {
-            throw Abort(.notFound)
-        }
-        
-        let readBody = try? getDomain(req)
-        print(readBody ?? "didn't parse the body")
-        
-        // Return the book
-        return author
-    }
-    
-    struct MyData: Content {
-        let pages: [String]
-    }
-
-    func getDomain(_ req: Request) throws -> [String] {
-        let data = try req.content.decode(MyData.self)
-        let response = Response(status: .ok)
-        try response.content.encode("Pages received: \(data.pages)")
-        return data.pages
-    }
+    //
     
     app.webSocket("") { req, ws in
         // Connected WebSocket.
@@ -103,4 +69,18 @@ func routes(_ app: Application) throws {
             print(binary)
         }
     }
+    
+    func parseWalletList(_ req: Request) throws -> [String] {
+        let data = try req.content.decode(WalletList.self)
+        return data.wallets
+    }
+    
+    func refreshHash(for token: String) {
+        // TODO:
+    }
+    
+    func refreshAllHashes() {
+        // TODO:
+    }
+
 }
